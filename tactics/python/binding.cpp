4dfbcc5fdbcdfb4fafeb9573f3f61bff2ca8cc66
wangaobo <kingaobo@gmail.com>
2024-08-28 15:22:13 +0800

    add print

--- tactics/python/binding.cpp
+++ tactics/python/binding.cpp
@@ -9,6 +9,8 @@
 /// This file defines the python binding
 ///
 //===--------------------------------------------------------------------------===//
+#include <iostream>
+#include <ostream>
 #include <pybind11/buffer_info.h>
 #include <pybind11/cast.h>
 #include <pybind11/detail/common.h>
@@ -16,7 +18,11 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pytypes.h>
 #include <pybind11/stl.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 #include <vector>
 #include "HalideRuntime.h"
 #include "tactics/core/tensor.h"
@@ -24,6 +30,7 @@
 
 namespace py = pybind11;
 using namespace tactics;
+using namespace fmt;
 
 template<typename T>
 Tensor* create_tensor(const std::vector<int> &shape, py::array_t<T> data) {
@@ -44,11 +51,111 @@ Tensor* create_tensor(const std::vector<int> &shape, py::array_t<T> data) {
   return Tensor::create(shape, ty, ptr);
 }
 
+std::string tensor_to_string(const Tensor &tensor) {
+  std::ostringstream oss;
+  auto data = tensor.host<float>();
+  auto shape = tensor.shape();
+  int total_elements = 1;
+  for (int dim : shape) {
+    total_elements *= dim;
+  }
+  int dimension = shape.size();
+  std::vector<int> counters(dimension, 0); // trace dimension index
+
+  // handler outer [
+  for (int i = 0; i < shape.size() - 1; ++i) {
+    if (counters[i] == 0) {
+      oss << "[";
+    }
+  }
+
+  for (int i = 0; i < total_elements; ++i) {
+
+    // print element
+    oss << data[i];
+
+    // update index counter
+    for (int j = shape.size() - 1; j >= 0; --j) {
+      counters[j]++;
+      if (counters[j] < shape[j])
+        break;
+      counters[j] = 0;
+      oss << "]";
+      if (j > 0) {
+        oss << ",\n";
+        for (int k = 0; k < j; ++k)
+          oss << " ";
+      }
+    }
+
+    // add ', '
+    if (i < total_elements - 1) {
+      oss << ", ";
+    }
+  }
+  oss << "]";
+
+  return oss.str();
+}
+
+void print_tensor_recursive(const Tensor &tensor, int depth = 0, int offset = 0) {
+  if (depth == tensor.shape().size() - 1) {
+    // print inner data
+    for (int i = 0; i < tensor.shape()[depth]; ++i) {
+      std::cout << tensor.host<float>()[offset + i] << " ";
+    }
+    std::cout << std::endl;
+  } else {
+    // recursive print high dimension element
+    int stride = 1;
+    for (int i = depth + 1; i < tensor.shape().size(); ++i) {
+      stride *= tensor.shape()[i];
+    }
+    for (int i = 0; i < tensor.shape()[depth]; ++i) {
+      print_tensor_recursive(tensor, depth + 1, offset + i * stride);
+    }
+    std::cout << std::endl; // end line
+  }
+}
+
+void print_tensor(const Tensor &tensor) {
+  int total_elements = 1;
+  for (int dim : tensor.shape()) {
+    total_elements *= dim;
+  }
+
+  int dimension = tensor.shape().size();
+  std::vector<int> counters(dimension, 0); // trace dimension index
+
+  for (int i = 0; i < total_elements; ++i) {
+    // print elements
+    std::cout << tensor.host<float>()[i] << " ";
+
+    // update index counter
+    for (int j = dimension - 1; j >= 0; --j) {
+      counters[j]++;
+      if (counters[j] < tensor.shape()[j]) {
+        break;
+      }
+      // reset current dimension index, end line
+      counters[j] = 0;
+      if (j > 0) {
+        std::cout << std::endl;
+      }
+    }
+  }
+  std::cout << std::endl;
+}
+
 PYBIND11_MODULE(tactics_bind, m) {
   py::class_<Tensor>(m, "Tensor")
     .def(py::init(&create_tensor<int>), py::arg("shape"), py::arg("data"))
     .def(py::init(&create_tensor<float>), py::arg("shape"), py::arg("data"))
-    .def("dump", [&](const Tensor& self) {
+    .def("matrix_dump", [&](const Tensor& self) {
       Matrix::print(&self);
-    });
+    })
+    .def("dump", [&](const Tensor& self) {
+      print_tensor_recursive(self);
+    })
+    .def("__str__", &tensor_to_string);
 }
